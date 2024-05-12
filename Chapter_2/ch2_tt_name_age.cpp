4dacf8b4bbf0b0708c13bc93fc9e9e7ec2d69abd
Sannidhya Roy Sadhya <72967411+Sadhya555@users.noreply.github.com>
2024-05-12 23:24:01 +0600

    Try This exercise "Name and Age" from Chapter 2 added

---
+++ Chapter_2/ch2_tt_name_age.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "Please enter your first name and age in years\n";
+	std::string first_name = "???";		// string variable named "first_name" ("???" means "we don't know the name currently")
+	double age = -1;					        // double variable named "age" ("-1" means "we don't know the age currently")
+	std::cin >> first_name >> age;		// inputting the string and integer values which will be read in that order
+	std::cout << "Hello, " << first_name << " (age: " << age*12 << " months)\n";		// Multiplying age by 12 to display the age in months instead of years
+
+}

d8ba54b7e5ec917d95f657ff64735121e4ea14b2
Sannidhya Roy Sadhya <72967411+Sadhya555@users.noreply.github.com>
2024-06-18 02:16:31 +0600

    Work in progress

--- Chapter_03/drill.cpp
+++ Chapter_03/drill.cpp
@@ -2,17 +2,50 @@
 
 int main()
 {
-    int a = 0;
-    int b = 0;
+    double a;
+    double b;
+    double smallest = 0;
+    double largest = 0;
     char input;
     char terminator = '|';
 
-    std::cout << "Enter two integers (or input | to quit)\n";
+    std::cout << "Enter the first value (or input | to quit)\n";
 
     while (true) {
-        if (std::cin >> a >> b) {
-            break;
+        if (std::cin >> a) {
+            std::cout << "Enter the second value\n";
+            std::cin >> b;
+            if (a < b) {
+                if (a - smallest <= 0) {
+                    a == smallest;
+                    std::cout << smallest << " is the smallest value so far\n";
+                    continue;
+                }
+                else {
+                    std::cout << "The smaller value is " << a << "\n";
+                }
+                if (largest - b) {
+                    largest = b;
+                    std::cout << largest << " is the largest value so far\n";
+                }
+                else {
+                    std::cout << "The larger value is " << b << "\n";
+                }
+                if (b - a < 0.01) {
+                    std::cout << "The numbers are almost equal\n";
+                }
+            }
+            else if (a > b) {
+                std::cout << "The smaller value is: " << b << "\nThe larger value is: " << a << "\n";
+                if (a - b < 0.01) {
+                    std::cout << "The numbers are almost equal\n";
+                }
+            }
+            else {
+                std::cout << "The numbers are equal\n";
+            }
         }
+
         else {
             std::cin.clear();
             std::cin >> input;
@@ -24,16 +57,6 @@ int main()
                 std::cout << "Invalid input. Enter two integers (or input | to quit)\n";
             }
         }
-    }
-
-    if (a < b) {
-        std::cout << "The smaller value is: " << a << "\nThe larger value is: " << b << "\n";
-    }
-    else if (a > b) {
-        std::cout << "The smaller value is: " << b << "\nThe larger value is: " << a << "\n";
-    }
-    else {
-        std::cout << "The numbers are equal\n";
     }
 
 }

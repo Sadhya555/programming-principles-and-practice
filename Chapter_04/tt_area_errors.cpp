8c76f5c27ef4c44e71b462ea50c1021efe1a0c1d
Sannidhya Roy Sadhya <72967411+Sadhya555@users.noreply.github.com>
2024-06-19 15:23:15 +0600

    Added file

---
+++ Chapter_04/tt_area_errors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int area(int length, int width) {
+    return length * width;
+}
+
+int main()
+{
+
+    int s1 = area(7, 2;
+    int s2 = area(7, 2)
+    Int s3 = area(7, 2);
+    int s4 = area('7, 2);
+
+} 

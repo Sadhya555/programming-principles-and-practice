8c59d60d3099fe986f8d7d5f6b8e5c81174863fd
Sannidhya Roy Sadhya <72967411+Sadhya555@users.noreply.github.com>
2024-07-02 07:40:50 +0600

    Added file

---
+++ Chapter_04/07_quadratic.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void roots(double a, double b, double c) {
+	double discriminant = (b * b) - (4 * a * c);
+	double result1, result2;
+
+	if (discriminant > 0) {
+		result1 = (-b + sqrt(discriminant)) / (2 * a);
+		result2 = (-b - sqrt(discriminant)) / (2 * a);
+		std::cout << result1 << " & " << result2;
+	}
+	else if (discriminant == 0) {
+		result1 = -b / (2 * a);
+		std::cout << result1;
+	}
+	else {
+		std::cout << "No real roots exist\n";
+	}
+	return 0;
+}
+
+int main() {
+	double a, b, c;
+	std::cout << "Enter the coefficients a, b, and c to calculate root(s)\n";
+	std::cin >> a >> b >> c;
+
+	if (a == 0) {
+		std::cerr << "ERROR! 'a' must be non-zero in a quadratic equation";
+		return -1;
+	}
+
+	roots(a, b, c);
+
+}

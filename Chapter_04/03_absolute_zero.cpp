51d46ce218d57a46e26c1c7691be84cb9a849177
Sannidhya Roy Sadhya <72967411+Sadhya555@users.noreply.github.com>
2024-07-03 15:01:29 +0600

    Updated

--- Chapter_04/03_absolute_zero.cpp
+++ Chapter_04/03_absolute_zero.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
-void error(std::string errortext) {
-    std::cerr << errortext << '\n';
+
+inline void error(const std::string& s) {
+    throw std::runtime_error(s);
+}
+
+inline void error(const std::string& s, const std::string& s2) {
+    error(s + s2);
+}
+
+inline void error(const std::string& s, int i) {
+    std::ostringstream os;
+    os << s << ": " << i;
+    error(os.str());
 }
 
 double ctok(double c) {
-    double k = c + 273.25;
+    double k = c + 273.15;
     return k;
 }
 
 int main() {
     double c = 0;
     std::cin >> c;
+
+    // Check if the input temperature is below absolute zero
     if (c < -273.15) {
-        error("Inavlid temperature!");
+        error("Invalid temperature! Temperature cannot be below -273.15°C.");
     }
     else {
         double k = ctok(c);
-        std::cout << k << '/n';
+        std::cout << k << '\n';
     }
+
 }
